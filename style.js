93337ad110acf430c56a40c3d070816d11f416db
Asqarch1k <asqarnorqulov464@gmail.com>
2023-08-18 11:12:59 +0500

    TODOList

---
+++ style.js
@@ -0,0 +1,78 @@
+const inputBox = document.querySelector("#inputBox");
+const listContainer = document.querySelector("#list-container");
+const time = document.querySelector(".time");
+const days = document.querySelector(".days");
+
+function addTask() {
+  if (inputBox.value === "") {
+    alert("You must write something");
+  } else {
+    let li = document.createElement("li");
+    li.innerHTML = inputBox.value;
+    listContainer.appendChild(li);
+    let span = document.createElement("span");
+    span.innerHTML = "\u00d7";
+    li.appendChild(span);
+  }
+  inputBox.value = "";
+  saveData();
+}
+
+listContainer.addEventListener(
+  "click",
+  function (e) {
+    if (e.target.tagName === "LI") {
+      e.target.classList.toggle("checked");
+      saveData();
+    } else if (e.target.tagName === "SPAN") {
+      e.target.parentElement.remove();
+      saveData();
+    }
+  },
+  false
+);
+
+function saveData() {
+  localStorage.setItem("data", listContainer.innerHTML);
+}
+function showTask() {
+  listContainer.innerHTML = localStorage.getItem("data");
+}
+showTask();
+
+function hozirgiKun() {
+  var date = new Date();
+  var yil = date.getFullYear();
+  var oy = date.getMonth() + 1;
+  var kun = date.getDate();
+
+  days.innerHTML =
+    "Sana: " +
+    (kun < 10 ? "0" + kun : kun) +
+    ":" +
+    (oy < 10 ? "0" + oy : oy) +
+    ":" +
+    (yil < 10 ? "0" + yil : yil) +
+    "y";
+}
+hozirgiKun();
+
+function hozirgiVaqtniOlish() {
+  var hozirgiVaqtniOlish = new Date();
+  var soat = hozirgiVaqtniOlish.getHours();
+  var daqiqa = hozirgiVaqtniOlish.getMinutes();
+  var saniya = hozirgiVaqtniOlish.getSeconds();
+  var oshibBorayotganSaniyalar = soat * 3600 + daqiqa * 60 + saniya;
+
+  time.innerHTML =
+    " Vaqt: " +
+    (soat < 10 ? "0" + soat : soat) +
+    ":" +
+    (daqiqa < 10 ? "0" + daqiqa : daqiqa) +
+    ":" +
+    (saniya < 10 ? "0" + saniya : saniya);
+}
+hozirgiVaqtniOlish();
+setInterval(function () {
+  hozirgiVaqtniOlish();
+}, 1000);
